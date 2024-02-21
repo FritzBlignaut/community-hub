033fea39d4dc23d8dcbf885c02534f7b3ef488b9
fritz <fritz.blignaut@gmail.com>
2024-02-21 19:44:33 +1000

    Created dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,5 @@ export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)},
     {path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)},
+    {path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent)},
 ];
