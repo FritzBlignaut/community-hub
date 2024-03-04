acec006ca4e860830ba07d75ab54d5f2d10db5a9
fritz <fritz.blignaut@gmail.com>
2024-03-04 16:23:22 +1000

    Added controls for job list

--- src/app/job-list/job-list.component.ts
+++ src/app/job-list/job-list.component.ts
@@ -1,12 +1,14 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'tss-job-list',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './job-list.component.html',
   styleUrl: './job-list.component.scss'
 })
 export class JobListComponent {
+  mockData = [{id: 1, dateCreated: '05/02/2024', description: 'Job 1 description', status: 'TODO'}, {id: 2, dateCreated: '06/03/2024', description: 'Job 2 description', status: 'TODO'}];
 
 }
